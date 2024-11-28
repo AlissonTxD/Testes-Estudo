5b1e62367062e668857227aba69be432d3cdab46
Alisson Notebook <omigadortxd@gmail.com>
2024-11-27 23:27:51 -0400

    Fix: Corrigidas strings

--- caminhoneiros.py
+++ caminhoneiros.py
@@ -4,15 +4,15 @@ encontrado = False
 
 def verifica_caminhao():
     global encontrado
-    placa_a_ser_verificada = str(input('coloque a placa que deseja buscar: '))
+    placa_a_ser_verificada = str(input("Coloque a placa do caminhão que deseja buscar: "))
     placa_a_ser_verificada = placa_a_ser_verificada.strip().upper()
     for caminhao in caminhoes:
         if placa_a_ser_verificada in caminhao['placa']:
             encontrado = True
-            print(f'Caminhão da placa {caminhao["placa"]} tem como motorista: {caminhao["motorista"]} - {caminhao["empresa"]}')
+            print(f"Caminhão da placa {caminhao["placa"]} tem como motorista: {caminhao["motorista"]} - {caminhao["empresa"]}.")
     if not encontrado:
-        print('Caminhâo nao encontrado')
+        print("Caminhão não encontrado.")
     encontrado = False
     verifica_caminhao()
 
-verifica_caminhao()   
+verifica_caminhao()
